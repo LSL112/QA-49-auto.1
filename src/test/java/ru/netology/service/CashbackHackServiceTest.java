7284f9723903e0fdbddf7120e26f2bd019dc2a01
Сергей Лешуков <lsl775319@gmail.com>
2023-02-04 21:51:27 +0300

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+
+    @Test
+    public void remain() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remain1() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+}
